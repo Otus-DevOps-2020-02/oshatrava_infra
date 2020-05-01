e490701ba88f187f001e3dce4c6b2e28ce041197
Oleg Shatrava <oleg.shatrava@gmail.com>
2020-05-01 23:51:07 +0300

    feat: split playbook and add new output vars for terraform

--- terraform/prod/outputs.tf
+++ terraform/prod/outputs.tf
@@ -1,3 +1,15 @@
 output "app_external_ip" {
   value = module.app.app_external_ip
 }
+
+output "app_local_ip" {
+  value = module.app.app_local_ip
+}
+
+output "db_external_ip" {
+  value = module.db.db_external_ip
+}
+
+output "db_local_ip" {
+  value = module.db.db_local_ip
+}
